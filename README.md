f112eab280d7b27770d9403b0a42eb86f6aebcc6
cessig <cessig4161994@gmail.com>
2020-09-01 12:32:47 -0400

    base javascrip game function with empty sumbit guess function and basic HTML structre no css at this point

--- README.md
+++ README.md
@@ -2,8 +2,29 @@
 
 Project Zero Hangman
 User Stories.
-I am going to be making a hangman game. On the screen there is going to be a user input for a letter to guess. As well as a ? button for the user to press to sumbit the guess. I will also have a trys left area where it will keep track of remaining tries the user has. I am going to have some styling that will add the shapes of a stick figure untill and wil add a body part everytime the user misses a word. When the user loses some text pops up on screen and says you have been hanged. If user wins some text appear on screen that you have won and not been hanged.
 
-My MVP is going to be very simple. I want to deleiver a product that simply has a box for the user to input a letter and five basic screen elements. I am going to try to make some catogeries i.e horror movies, marvel superheros for the user to select and then randomly generate a title or name from the category they selected. I am going to put the contents of the theme into the array. Which once the user picks a theme will randomly generate a title or hero from the array.
+- I am going to be making a hangman game.
+- start button
+- display a hint of the word generated
+- On the screen there is going to be a user input for a letter to guess.
+- As well as a sumbit button for the user to press to sumbit the guess.
+- I am going to have some images for each wrong anser
+- When the user loses some text pops up on screen and says you have been hanged.
+- If user wins some text appear on screen that you have won and not been hanged.
+- use words that are more then five letters wrong.
 
-My stretch goals would be to make the game look pretty. Maybe have five different cattegories. Some good visiual animation on the hangman would be a strech goal as well.
+- My MVP is going to be very simple.
+
+2.  I want to deleiver a product that simply has a box for the user to input a letter.
+
+- If the letter is wrong add a image
+- if the letter is correct display it in the area for the word to appear
+
+3.  six screen elements.
+4.  1 array of marvel superheros to loop thru to randomly generates a hero.
+5.  Display a hint on the screen once selected.
+
+- My stretch goals would be to make the game look pretty.
+- Maybe have five different cattegories.
+- Some good visiual animation on the hangman would be a strech goal as well.
+- multiple themes
---
+++ app.js
@@ -0,0 +1,143 @@
+// An array of superHeros including there name and a hint to prompt the user.
+const superHeros = [
+  {
+    name: "Wolverine",
+    hint: "member of xmen and smokes cigars",
+  },
+  {
+    name: "Spiderman",
+    hint: "lover is mary jane parker",
+  },
+  {
+    name: "Ironman",
+    hint: "billionaire who owns a company with industries in the name",
+  },
+  {
+    name: "CaptainAmerica",
+    hint: "soldier from world war 2",
+  },
+  {
+    name: "Deadpool",
+    hint: "Classic Anti-Hero known for its recent R rated films",
+  },
+  {
+    name: "ProfessorX",
+    hint: "runs a school for mutants",
+  },
+  {
+    name: "Antman",
+    hint: "gets really really really small for his superpower",
+  },
+  {
+    name: "BlackPanther",
+    hint: "lives in wakanda",
+  },
+  {
+    name: "Storm",
+    hint: "controls the weather",
+  },
+  {
+    name: "Starlord",
+    hint: "chris pratt",
+  },
+  {
+    name: "Thanos",
+    hint: "all things must be balanced",
+  },
+  {
+    name: "Punisher",
+    hint: "on a quest for brutal vengance after his family is killed",
+  },
+  {
+    name: "Hawkeye",
+    hint: "Bow and arrow is this heros weapon of choice",
+  },
+  {
+    name: "Groot",
+    hint: "arborial super hero",
+  },
+  {
+    name: "NickFury",
+    hint: "leader of SHIELD",
+  },
+  {
+    name: "IncredibleHulk",
+    hint: "Don't wamt tp make this guy angry",
+  },
+  {
+    name: "DoctorStrange",
+    hint: "cape acts as a third set off hands",
+  },
+  {
+    name: "Daredevil",
+    hint: "blind but does not let that stop him",
+  },
+  {
+    name: "Blackwidow",
+    hint: "This female hero was orginally a villian",
+  },
+  {
+    name: "BruceBanner",
+    hint: "the calm version of something you dont want to get angry",
+  },
+];
+
+/*varaibles i think i will need.
+guessInput
+guessButton
+lettersGuessed
+matchedLetters
+superHint
+heros
+livesLeft
+
+ */
+
+//Randomly selecting a superhero to pull its name and hint and store it.
+// starting the game
+let game = null;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function startGame() {
+  game = {
+    wrongGuesses: 0,
+    matchedLetters: [], // storage for letters of the word this game
+    guessLetters: [],
+    randomHero: null,
+    maxGuesses: 5,
+    imageDisplayed: null,
+  };
+
+  game.randomHero = superHeros[getRandomIntInclusive(0, superHeros.length - 1)];
+
+  game.matchedLetters = Array(game.randomHero.name.length).fill("_");
+
+  for (let index = 0; index < game.matchedLetters.length; index++) {
+    const matchedLetter = game.matchedLetters[index];
+    const letterElement = document.createElement("span");
+    letterElement.innerHTML = matchedLetter;
+    const parentElement = $("#letterDisplay");
+    parentElement.append(letterElement);
+  }
+}
+
+function sumbitGuess() {}
+
+// Display Length of word to the user
+
+//  if Right Guesses less then the length of the word
+
+// prompt the user to make another guess
+
+// if the guess is correct display the letter on screen to the user
+
+// if the guess is incorrect pop a image on scree.
+
+// if the incorrect guesses is greater then or equal to 5 tell the user that they lost and to exit/restart
+
+// If the correct guesses is equal to the length of the word. The user has won and promp them to exit/restart.
